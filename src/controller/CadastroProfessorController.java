bd7720e67c74901a79f114c7b867375f69fa4477
KennedyAlvess <kenedy199717@gmail.com>
2023-05-03 13:44:40 -0300

    Att das classes controller

--- src/controller/CadastroProfessorController.java
+++ src/controller/CadastroProfessorController.java
@@ -1,8 +1,10 @@
 package controller;
 
 import java.net.URL;
+import java.sql.Connection;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,15 +12,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import model.dao.ProfessorDAO;
+import model.database.Database;
+import model.database.DatabasePostgreSQL;
 import model.entidades.Professor;
 
 public class CadastroProfessorController implements Initializable{
 
     /*Incluido todos os componentes criados na Interface Grafica como atributos na classe */
     @FXML
-    private TableView<Professor> tableProfessores;
+    private TableView<Professor> tableViewProfessores;
     @FXML
-    private TableColumn<Professor, String> tableNomeProfessores;
+    private TableColumn<Professor,String> colunaNomeProfessores;
+    @FXML
+    private TableColumn<Professor,String> colunaMatProfessores;
     @FXML
     private Button botaoAlterar;
     @FXML
@@ -31,17 +40,29 @@ public class CadastroProfessorController implements Initializable{
     private Label labelProfessorTitulacao;
     @FXML
     private Label labelProfessorEmail;
-    @FXML
-    private Label labelProfessorTelefone;
 
     private List<Professor> listProfessores; 
     private ObservableList<Professor> observableListProfessores;
 
-
-
+    //Manipulando banco de dados
+    private final Database database = new DatabasePostgreSQL();
+    private final Connection connection = database.conectar();
+    private final ProfessorDAO professorDAO = new ProfessorDAO();
+    
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-       
+    public void initialize(URL url, ResourceBundle resources) {
+        professorDAO.setConnection(connection);
+        carregarTableViewProfessor();
+    }
+
+    public void carregarTableViewProfessor(){
+        colunaNomeProfessores.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        colunaMatProfessores.setCellValueFactory(new PropertyValueFactory<>("matricula"));
+
+        listProfessores = professorDAO.listar();
+
+        observableListProfessores = FXCollections.observableArrayList(listProfessores);
+        tableViewProfessores.setItems(observableListProfessores);
     }
     
 }
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,7 +1,11 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
@@ -25,5 +29,11 @@ public class MainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
+    }
+
+    @FXML
+    public void handleMenuCadastroProfessor() throws IOException{
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("view/CadastroProfessorMenu.fxml"));
+        anchorPane.getChildren().setAll(a);
     }
 }
