cf802e85ffa616523229a384d8be7fc4e4ffbc46
KennedyAlvess <kenedy199717@gmail.com>
2023-05-04 09:33:21 -0300

    Correção de erros da conexão com banco de dados

--- src/model/database/DatabasePostgreSQL.java
+++ src/model/database/DatabasePostgreSQL.java
@@ -3,8 +3,7 @@ package model.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
  
 public class DatabasePostgreSQL implements Database {
