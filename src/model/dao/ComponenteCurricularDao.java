e2036e2fddaf32ddb8fce1b2d21a3f08f1be2365
KennedyAlvess <kenedy199717@gmail.com>
2023-05-04 17:20:45 -0300

    implementação da classe ComponenteCurricular

--- src/model/dao/ComponenteCurricularDao.java
+++ src/model/dao/ComponenteCurricularDao.java
@@ -1,5 +1,135 @@
 package model.dao;
 
-public class ComponenteCurricularDao {
-    
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import model.entidades.ComponenteCurricular;
+
+public class ComponenteCurricularDAO {
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean inserir(ComponenteCurricular componentecurricular) {
+        String sql = "INSERT INTO componentecurricular(nomecc, cargahorariacc, codigo,semestre,obrigatoriedade) VALUES(?,?,?,?,?)";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, componentecurricular.getNome());
+            stmt.setInt(2, componentecurricular.getCargaHoraria());
+            stmt.setString(3, componentecurricular.getCodigo());
+            stmt.setInt(4, componentecurricular.getSemestre());
+            stmt.setString(5, componentecurricular.getObrigatoriedade());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ComponenteCurricularDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean alterar(ComponenteCurricular componentecurricular) {
+        String sql = "UPDATE componentecurriculares SET nomecc=?, cargahorariacc=?, semestre=?,obrigatoriedade=? WHERE codigo=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, componentecurricular.getNome());
+            stmt.setInt(2, componentecurricular.getCargaHoraria());
+            stmt.setInt(3, componentecurricular.getSemestre());
+            stmt.setString(4, componentecurricular.getObrigatoriedade());
+            stmt.setString(5, componentecurricular.getCodigo());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ComponenteCurricularDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean remover(ComponenteCurricular componentecurricular) {
+        String sql = "DELETE FROM componentecurricular WHERE codigo=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, componentecurricular.getCodigo());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ComponenteCurricularDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public List<ComponenteCurricular> listar() {
+        String sql = "Select * from ComponenteCurriculares order by nome";
+        List<ComponenteCurricular> retorno = new ArrayList<>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery();
+            while (resultado.next()) {
+                ComponenteCurricular componentecurricular = new ComponenteCurricular();
+                componentecurricular.setNome(resultado.getString("nomecc"));
+                componentecurricular.setCargaHoraria(resultado.getInt("cargahorariacc"));
+                componentecurricular.setCodigo(resultado.getString("codigo"));
+                componentecurricular.setSemestre(resultado.getInt("semestre"));
+                componentecurricular.setObrigatoriedade(resultado.getString("obrigatoriedade"));
+                retorno.add(componentecurricular);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro ao listar os ComponenteCurriculares");
+        }
+        return retorno;
+    }
+
+    public ComponenteCurricular buscar(ComponenteCurricular componentecurricular) {
+        String sql = "SELECT * FROM componentecurriculares WHERE codigo=?";
+        ComponenteCurricular retorno = new ComponenteCurricular();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, componentecurricular.getCodigo());
+            ResultSet resultado = stmt.executeQuery();
+            if (resultado.next()) {
+                componentecurricular.setNome(resultado.getString("nomecc"));
+                componentecurricular.setCargaHoraria(resultado.getInt("cargahorariacc"));
+                componentecurricular.setCodigo(resultado.getString("codigo"));
+                componentecurricular.setSemestre(resultado.getInt("semestre"));
+                componentecurricular.setObrigatoriedade(resultado.getString("obrigatoriedade"));             
+                retorno = componentecurricular;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ComponenteCurricularDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retorno;
+    }
+
+    public List<String> codigo() {
+        String sql = "SELECT codigo FROM componentecurriculares";
+        List<String> matriculas = new ArrayList<>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery();
+            while (resultado.next()) {
+                ComponenteCurricular componentecurricular = new ComponenteCurricular();
+                componentecurricular.setCodigo(resultado.getString("codigo"));
+                matriculas.add(componentecurricular.getCodigo());
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ComponenteCurricularDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return matriculas;
+    }
 }
+
+
+
+
+
--- src/model/entidades/ComponenteCurricular.java
+++ src/model/entidades/ComponenteCurricular.java
@@ -1,56 +1,45 @@
 package model.entidades;
 
 import java.io.Serializable;
-import java.util.Iterator;
-import java.util.LinkedList;
 
 
 
 public class ComponenteCurricular implements Serializable {
     private String nome;
-    private short cargaHoraria;
+    private int cargaHoraria;
     private String codigo;
-    private String preRequisito;
-    private String modalidade;
-    private LinkedList<String> equivalentes;
+    private int semestre;
+    private String obrigatoriedade;
+
  
 
     public ComponenteCurricular() {
     }
 
-    public ComponenteCurricular(String nome, short cargaHoraria, String codigo, String modalidade) {
+    public ComponenteCurricular(String nome,String codigo, int cargaHoraria, int semestre,String obrigatoriedade) {
+        this.semestre = semestre;
+        this.obrigatoriedade = obrigatoriedade;
         this.nome = nome;
         this.cargaHoraria = cargaHoraria;
         this.codigo = codigo;
-        this.modalidade = modalidade;
     }
 
-    public ComponenteCurricular(String nome, short cargaHoraria, String codigo, String modalidade,
-            LinkedList<String> equivalentes) {
-        this.nome = nome;
-        this.cargaHoraria = cargaHoraria;
-        this.codigo = codigo;
-        this.modalidade = modalidade;
-        this.equivalentes = equivalentes;
+
+
+    public int getSemestre() {
+        return semestre;
     }
 
-    public ComponenteCurricular(String nome,String codigo, short cargaHoraria, String preRequisito, String modalidade) {
-        this.nome = nome;
-        this.cargaHoraria = cargaHoraria;
-        this.codigo = codigo;
-        this.preRequisito = preRequisito;
-        this.modalidade = modalidade;
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
     }
-    public ComponenteCurricular(String nome, short cargaHoraria, String codigo, String preRequisito, String modalidade, String[] equivalentes) {
-        this.nome = nome;
-        this.cargaHoraria = cargaHoraria;
-        this.codigo = codigo;
-        this.preRequisito = preRequisito;
-        this.modalidade = modalidade;
-        this.equivalentes = new LinkedList<>();
-        for (int i = 0; i < equivalentes.length; i++) {
-            addEquivalente(equivalentes[i]);
-        }
+
+    public String getObrigatoriedade() {
+        return obrigatoriedade;
+    }
+
+    public void setObrigatoriedade(String obrigatoriedade) {
+        this.obrigatoriedade = obrigatoriedade;
     }
 
     public String getNome() {
@@ -61,12 +50,12 @@ public class ComponenteCurricular implements Serializable {
         this.nome = nome;
     }
 
-    public short getCargaHoraria() {
+    public int getCargaHoraria() {
         return cargaHoraria;
     }
 
-    public void setCargaHoraria(short cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria += cargaHoraria;
     }
 
     public String getCodigo() {
@@ -75,40 +64,6 @@ public class ComponenteCurricular implements Serializable {
 
     public void setCodigo(String codigo) {
         this.codigo = codigo;
-    }
-
-    public String getPreRequisito() {
-        return preRequisito;
-    }
-
-    public void setPreRequisito(String preRequisito) {
-        this.preRequisito = preRequisito;
-    }
-
-    public String getmodalidade() {
-        return modalidade;
-    }
-
-    public void setmodalidade(String modalidade) {
-        this.modalidade = modalidade;
-    }
-
-    public LinkedList<String> getEquivalentes() {
-        return equivalentes;
-    }
-
-    public void addEquivalente(String equivalente){
-        this.equivalentes.add(equivalente);
-    }
-
-    public void removerEquivalente(String equivalente){
-        Iterator<String> iteratorEquivalentes = equivalentes.iterator();
-        while(iteratorEquivalentes.hasNext()){
-            if(iteratorEquivalentes.next() == equivalente ){
-                iteratorEquivalentes.remove();
-                break;
-            }
-        }
     }
 
 }
