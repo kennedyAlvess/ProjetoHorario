636c3fd4a83513a033c3bcd2d2accaa805a26a4d
KennedyAlvess <kenedy199717@gmail.com>
2023-05-04 09:34:09 -0300

    Pequenas att

--- src/controller/CadastroProfessorController.java
+++ src/controller/CadastroProfessorController.java
@@ -1,68 +1,53 @@
 package controller;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.util.List;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TextField;
 
-import model.dao.ProfessorDAO;
-import model.database.Database;
-import model.database.DatabasePostgreSQL;
-import model.entidades.Professor;
+public class CadastroProfessorController implements Initializable {
+    @FXML
+    private TextField TextFieldProfessorEmail;
 
-public class CadastroProfessorController implements Initializable{
+    @FXML
+    private TextField TextFieldProfessorMat;
 
-    /*Incluido todos os componentes criados na Interface Grafica como atributos na classe */
     @FXML
-    private TableView<Professor> tableViewProfessores;
+    private TextField TextFieldProfessorNome;
+
     @FXML
-    private TableColumn<Professor,String> colunaNomeProfessores;
+    private TextField TextFieldProfessorTitulo;
+
     @FXML
-    private TableColumn<Professor,String> colunaMatProfessores;
+    private Button botaoCancelar;
+
     @FXML
-    private Button botaoAlterar;
-    @FXML
-    private Button botaoRemover;
-    @FXML
-    private Label labelProfessorNome;
-    @FXML
-    private Label labelProfessorMatricula;
-    @FXML
-    private Label labelProfessorTitulacao;
+    private Button botaoConfirmar;
+
     @FXML
     private Label labelProfessorEmail;
 
-    private List<Professor> listProfessores; 
-    private ObservableList<Professor> observableListProfessores;
+    @FXML
+    private Label labelProfessorMat;
 
-    //Manipulando banco de dados
-    private final Database database = new DatabasePostgreSQL();
-    private final Connection connection = database.conectar();
-    private final ProfessorDAO professorDAO = new ProfessorDAO();
+    @FXML
+    private Label labelProfessorNome;
+
+    @FXML
+    private Label labelProfessorTitulo;
+
     
+
+
     @Override
-    public void initialize(URL url, ResourceBundle resources) {
-        professorDAO.setConnection(connection);
-        carregarTableViewProfessor();
+    public void initialize(URL location, ResourceBundle resources) {
+        
     }
 
-    public void carregarTableViewProfessor(){
-        colunaNomeProfessores.setCellValueFactory(new PropertyValueFactory<>("nome"));
-        colunaMatProfessores.setCellValueFactory(new PropertyValueFactory<>("matricula"));
 
-        listProfessores = professorDAO.listar();
 
-        observableListProfessores = FXCollections.observableArrayList(listProfessores);
-        tableViewProfessores.setItems(observableListProfessores);
-    }
-    
 }
---
+++ src/controller/CadastroProfessorMenuController.java
@@ -0,0 +1,82 @@
+package controller;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import model.dao.ProfessorDAO;
+import model.database.Database;
+import model.database.DatabasePostgreSQL;
+import model.entidades.Professor;
+
+public class CadastroProfessorMenuController implements Initializable{
+
+    /*Incluido todos os componentes criados na Interface Grafica como atributos na classe */
+    @FXML
+    private TableView<Professor> tableViewProfessor;
+    @FXML
+    private TableColumn<Professor,String> colunaNomeProfessor;
+    @FXML
+    private Button botaoAlterar;
+    @FXML
+    private Button botaoRemover;
+    @FXML
+    private Label labelProfessorNome;
+    @FXML
+    private Label labelProfessorMatricula;
+    @FXML
+    private Label labelProfessorTitulacao;
+    @FXML
+    private Label labelProfessorEmail;
+
+    private List<Professor> listProfessores; 
+    private ObservableList<Professor> observableListProfessores;
+
+    //Manipulando banco de dados
+    private final Database database = new DatabasePostgreSQL();
+    private final Connection connection = database.conectar();
+    private final ProfessorDAO professorDAO = new ProfessorDAO();
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resources) {
+        professorDAO.setConnection(connection);
+        carregarTableViewProfessor();
+        tableViewProfessor.getSelectionModel().selectedItemProperty().addListener((observable,oldValue,newValue
+        ) -> selecionarItemTableViewProfessor(newValue));
+    }
+
+    public void selecionarItemTableViewProfessor(Professor professor){
+        if (professor != null) {
+            labelProfessorNome.setText(professor.getNome());
+            labelProfessorMatricula.setText(String.valueOf(professor.getMatricula()));
+            labelProfessorTitulacao.setText(professor.getTitulacao());
+            labelProfessorEmail.setText(professor.getEmail());          
+        } else {
+            labelProfessorNome.setText("");
+            labelProfessorMatricula.setText("");
+            labelProfessorTitulacao.setText("");
+            labelProfessorEmail.setText("");
+        }
+
+    }
+
+    public void carregarTableViewProfessor(){
+        colunaNomeProfessor.setCellValueFactory(new PropertyValueFactory<>("nome"));
+
+        listProfessores = professorDAO.listar();
+
+        observableListProfessores = FXCollections.observableArrayList(listProfessores);
+        tableViewProfessor.setItems(observableListProfessores);
+    }
+    
+}
