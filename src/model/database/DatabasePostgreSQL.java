9b5cb2b1e55aadbb8788e9c1ac98613e63b58ad3
KennedyAlvess <kenedy199717@gmail.com>
2023-05-10 11:58:46 -0300

    Conexão com o banco de dados

--- src/model/database/DatabasePostgreSQL.java
+++ src/model/database/DatabasePostgreSQL.java
@@ -12,7 +12,7 @@ public class DatabasePostgreSQL implements Database {
     public Connection conectar() {
         try {
             Class.forName("org.postgresql.Driver");
-            this.connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/<nome do seu banco de dados>, "user","senha");
+            this.connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BDprojeto,prostgres,19738246");
             return this.connection;
         } catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Drive não encontrado ou erro na conexao com o banco de dados");
