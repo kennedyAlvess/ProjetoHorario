ad968dad7ddd4f304bf8aa5726eed241024e2f1e
KennedyAlvess <kenedy199717@gmail.com>
2023-09-26 19:48:18 -0300

    Main

---
+++ src/App.java
@@ -0,0 +1,24 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class App extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public  void start(Stage Stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/VboxMenu.fxml"));
+        Scene home = new Scene(root);
+        Stage.setScene(home);
+        Stage.setTitle("Melhor que o SIGAA");
+        Stage.setResizable(false);
+        Stage.show();
+    }
+
+}
